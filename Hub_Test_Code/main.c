5ea94d1f5045b314d0db28b1c62aca79dfa6a603
Aaron Ewing <aaronewing@nnu.edu>
2016-08-20 15:58:26 -0600

    created Hub Test Code

---
+++ Hub_Test_Code/main.c
@@ -0,0 +1,10 @@
+/* Code is for testing Hub functionality with radio
+ * after a vibe test. The code is supposed to turn on LED(s)
+ * while running code, and transmit a single packet to the radio
+ */
+
+// main.c
+int main(void) {
+	
+	return 0;
+}
