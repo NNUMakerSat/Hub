4e0f3eeb731e0b5c36ea6942821e2220b5d122f7
Aaron Ewing <aaronewing@nnu.edu>
2016-08-23 22:03:47 -0600

    trying to get radio to work

--- Hub_Functionality_Testing/LED.c
+++ Hub_Functionality_Testing/LED.c
@@ -25,75 +25,154 @@ void LED_2_On(void) {
 }
 
 void LED_1_Off(void) {
-	P1OUT &= ~BIT0;						// Turn off LED1
 }
 
 void LED_2_Off(void) {
 	P9OUT &= ~BIT7;					// Turn off LED2
 }
 
-void flash_LED_1(uint8_t clk_Delay1, uint32_t duration1) {
-	for (g_timer1 = 0; g_timer1 < duration1; ++g_timer1) {	// Flash LED however long you want at whatever frequency you want
-		P1OUT |= BIT0;
-		switch (clk_Delay1){
-		case 0:
-			__delay_cycles(100000);							// Delay to 160Hz/10Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 1:
-			__delay_cycles(1000000);						// Delay to 16Hz/1Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 2:
-			__delay_cycles(10000000);						// Delay to 1.6Hz/0.1Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 3:
-			__delay_cycles(100000000);						// Delay to 0.16Hz/0.01Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 4:
-			__delay_cycles(1600000);						// Delay to 10Hz/0.625Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 5:
-			__delay_cycles(16000000);						// Delay to 1Hz/0.0625Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 6:
-			__delay_cycles(160000000);						// Delay to 0.1Hz/0.00625Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 7:
-			__delay_cycles(1600000000);						// Delay to 0.01Hz/0.000625Hz (16MHz clk vs. 1MHz clk)
-			break;
+void flash_LED_1(uint8_t clk_Delay1, uint32_t duration1) {		// Flash LED however long you want at whatever frequency you want
+	uint8_t i;
+	for (g_timer1 = 0; g_timer1 < duration1; ++g_timer1) {
+		if (!(P1OUT == BIT0)) {									// If LED 2 is off
+			for (i = 0; i < 2; ++i) {
+				P1OUT ^= BIT0;
+				switch (clk_Delay1){
+				case 0:
+					__delay_cycles(100000);							// Delay to 160Hz/10Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 1:
+					__delay_cycles(1000000);						// Delay to 16Hz/1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 2:
+					__delay_cycles(10000000);						// Delay to 1.6Hz/0.1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 3:
+					__delay_cycles(100000000);						// Delay to 0.16Hz/0.01Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 4:
+					__delay_cycles(1600000);						// Delay to 10Hz/0.625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 5:
+					__delay_cycles(16000000);						// Delay to 1Hz/0.0625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 6:
+					__delay_cycles(160000000);						// Delay to 0.1Hz/0.00625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 7:
+					__delay_cycles(1600000000);						// Delay to 0.01Hz/0.000625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				default:
+					__delay_cycles(20000);							// Delay to 1600Hz/500Hz (16MHz clk vs. 1MHz clk)
+					break;
+				}
+			}
+		} else {
+			for (i = 0; i < 3; ++i) {
+				P1OUT ^= BIT0;
+				switch (clk_Delay1) {
+				case 0:
+					__delay_cycles(100000);							// Delay to 160Hz/10Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 1:
+					__delay_cycles(1000000);						// Delay to 16Hz/1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 2:
+					__delay_cycles(10000000);						// Delay to 1.6Hz/0.1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 3:
+					__delay_cycles(100000000);						// Delay to 0.16Hz/0.01Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 4:
+					__delay_cycles(1600000);						// Delay to 10Hz/0.625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 5:
+					__delay_cycles(16000000);						// Delay to 1Hz/0.0625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 6:
+					__delay_cycles(160000000);						// Delay to 0.1Hz/0.00625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 7:
+					__delay_cycles(1600000000);						// Delay to 0.01Hz/0.000625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				default:
+					__delay_cycles(20000);							// Delay to 1600Hz/500Hz (16MHz clk vs. 1MHz clk)
+					break;
+				}
+			}
 		}
-		P1OUT &= ~BIT0;
 	}
 }
 
-void flash_LED_2(uint8_t clk_Delay2, uint32_t duration2) {
-	for (g_timer2 = 0; g_timer2 < duration2; ++g_timer2) {	// Flash LED however long you want at whatever frequency you want
-		P9OUT |= BIT7;
-		switch (clk_Delay2){
-		case 0:
-			__delay_cycles(100000);							// Delay to 160Hz/10Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 1:
-			__delay_cycles(1000000);						// Delay to 16Hz/1Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 2:
-			__delay_cycles(10000000);						// Delay to 1.6Hz/0.1Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 3:
-			__delay_cycles(100000000);						// Delay to 0.16Hz/0.01Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 4:
-			__delay_cycles(1600000);						// Delay to 10Hz/0.625Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 5:
-			__delay_cycles(16000000);						// Delay to 1Hz/0.0625Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 6:
-			__delay_cycles(160000000);						// Delay to 0.1Hz/0.00625Hz (16MHz clk vs. 1MHz clk)
-			break;
-		case 7:
-			__delay_cycles(1600000000);						// Delay to 0.01Hz/0.000625Hz (16MHz clk vs. 1MHz clk)
-			break;
+void flash_LED_2(uint8_t clk_Delay2, uint32_t duration2) {	// Flash LED however long you want at whatever frequency you want
+	uint8_t i;
+	for (g_timer2 = 0; g_timer2 < duration2; ++g_timer2) {
+		if (!(P9OUT == BIT7)) {									// If LED 2 is off
+			for (i = 0; i < 2; ++i) {
+				P9OUT ^= BIT7;
+				switch (clk_Delay2){
+				case 0:
+					__delay_cycles(100000);							// Delay to 160Hz/10Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 1:
+					__delay_cycles(1000000);						// Delay to 16Hz/1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 2:
+					__delay_cycles(10000000);						// Delay to 1.6Hz/0.1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 3:
+					__delay_cycles(100000000);						// Delay to 0.16Hz/0.01Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 4:
+					__delay_cycles(1600000);						// Delay to 10Hz/0.625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 5:
+					__delay_cycles(16000000);						// Delay to 1Hz/0.0625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 6:
+					__delay_cycles(160000000);						// Delay to 0.1Hz/0.00625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 7:
+					__delay_cycles(1600000000);						// Delay to 0.01Hz/0.000625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				default:
+					__delay_cycles(20000);							// Delay to 1600Hz/500Hz (16MHz clk vs. 1MHz clk)
+					break;
+				}
+			}
+		} else {
+			for (i = 0; i < 3; ++i) {
+				P9OUT ^= BIT7;
+				switch (clk_Delay2){
+				case 0:
+					__delay_cycles(100000);							// Delay to 160Hz/10Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 1:
+					__delay_cycles(1000000);						// Delay to 16Hz/1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 2:
+					__delay_cycles(10000000);						// Delay to 1.6Hz/0.1Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 3:
+					__delay_cycles(100000000);						// Delay to 0.16Hz/0.01Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 4:
+					__delay_cycles(1600000);						// Delay to 10Hz/0.625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 5:
+					__delay_cycles(16000000);						// Delay to 1Hz/0.0625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 6:
+					__delay_cycles(160000000);						// Delay to 0.1Hz/0.00625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				case 7:
+					__delay_cycles(1600000000);						// Delay to 0.01Hz/0.000625Hz (16MHz clk vs. 1MHz clk)
+					break;
+				default:
+					__delay_cycles(20000);							// Delay to 1600Hz/500Hz (16MHz clk vs. 1MHz clk)
+					break;
+				}
+			}
 		}
-		P9OUT &= ~BIT7;
 	}
 }
--- Hub_Functionality_Testing/main.c
+++ Hub_Functionality_Testing/main.c
@@ -13,6 +13,9 @@
 // main.c
 int main(void) {
 	uint8_t i;				// counter for loop
+	uint8_t j = 0;
+	uint8_t RX_Data;
+	uint8_t collect_Array[39];
 	uint8_t test_Array[39] = {0x50, 0x50, 0x50, 0x0C, 'D', 'e', 'a', 'r', ' ', 'E', 'L', 'a', 'N', 'a', ' ', 'X', 'I', 'V', ',',
 			' ', 'M', 'a', 'k', 'e', 'r', 'S', 'a', 't', '-', '0', ' ', 'i', 's', ' ', 'r', 'e', 'a', 'd', 'y'};
 			// 4 control bytes + "Dear ELaNa XIV, MakerSat-0 is ready"
@@ -22,17 +25,43 @@ int main(void) {
 	initialize_Ports();
 	initialize_Clocks();
 	init_LED();
-
-	LED_1_On();								// Turn LED 1 on, indicating power
-
 	init_UART(0, 0);						// Initalize UART at Baud = 9600 with P2.0 - TX, P2.1 - RX, P1.4 - Radio Busy line
 
-	for (i = 0; i < 39; ++i) {			// Writes test_Array to radio
+	for (i = 0; i < 39; ++i) {				// Writes test_Array to radio
 		write_UART(test_Array[i]);
-//		flash_LED_2(0, 1);				// Flash LED 2 at 10Hz once
-	}									// 0 - 10Hz, 1 - Hz
+		flash_LED_1(10, 1);					// Flash LED 1 at 500 Hz (default) once for every byte through TX
 
+	for (j = 0; j < 39; ++j) {				// Writes test_Array to radio
+		read_UART();
+		flash_LED_2(0, 1);					// Flash LED 2 at 10Hz once
+		collect_Array[j] = RX_Data;			// 0 - 10Hz, 1 - Hz
+	}
+/*
+	while (1) {								//reads what emulator puts in.
+		read_UART();
+		collect_Array[j] = RX_Data;
+		++j;
+	}
+*/
+
+/*	for (i = 0; i < 39; ++i) {			// Writes test_Array to radio
+			write_UART(test_Array[i]);
+	//		flash_LED_2(0, 1);				// Flash LED 2 at 10Hz once
+//			read_UART();
+	//		collect_Array[j] = RX_Data;
+		//	++j;
+		}									// 0 - 10Hz, 1 - Hz
+
+		while (1) {							//reads what emulator puts in.
+			read_UART();
+			collect_Array[j] = RX_Data;
+			++j;
+			LED_2_On();
+		}
+
+*/
 	while (1) {
-	flash_LED_2(1, 1);				// Flash LED 2 at 1HZ forever after UART TX
+	flash_LED_1(1, 1);				// Flash LED 1 & 2 at 1HZ forever after UART TX
+	flash_LED_2(1, 1);
 	}
 }
